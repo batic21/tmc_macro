5e1ef64e7dd48494f4b5f5f88074da738a75702f
batic21 <batic21@gmail.com>
2023-01-13 21:54:17 +0800

    change to individual file macro search

--- macro_search.py
+++ macro_search.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
+import xlsxwriter
 result_macroname =[]
 result_filename = []
 result_line_number = []
 result_csvfile = []
-import xlsxwriter
+
 
 M_TRUE = 1
 M_FALSE = 0
@@ -30,9 +31,15 @@ def process_file(fname):
     m_row_count = len(df)
     progress = 0
     progress_div = float(100/m_row_count)
-    prg_crt=0
-        
+    prg_crt=0        
     temp_duplicate_macro = 0
+
+    result_macroname.clear()
+    result_filename.clear()
+    result_line_number.clear()
+    result_csvfile.clear()
+    dup_found.clear()
+    dfoundidx = 0
 
     
     for r in range(m_row_count):
@@ -49,7 +56,7 @@ def process_file(fname):
        if dfound == M_TRUE:   
            #print("======duplicate found  index = " + str(dindex) + "found index" + str(dfoundidx))    
            #print(result_filename) 
-           result_csvfile[dindex].append([fname])
+           #result_csvfile[dindex].append([fname])
            result_filename[dindex].append(os.path.basename(str(df.loc[r][1])))
            result_line_number[dindex].append(df.loc[r][2])                       
            
@@ -63,8 +70,8 @@ def process_file(fname):
                                                          
        else:
            result_macroname.append(str(df.loc[r][0]))
-           result_csvfile.append([fname])
-           tidx = result_macroname.index(str(df.loc[r][0]))
+           #result_csvfile.append([fname])
+           #tidx = result_macroname.index(str(df.loc[r][0]))
            result_filename.append([os.path.basename(str(df.loc[r][1]))])
            result_line_number.append([df.loc[r][2]])
         
@@ -75,36 +82,46 @@ def process_file(fname):
     print("")
 
 
-def macro_export(mname, mfile, mline):
-    workbook = xlsxwriter.Workbook('result.xlsx')
+def macro_export(mname, mfile, mline, excel_fname):
+    workbook = xlsxwriter.Workbook('result_' + excel_fname + '.xlsx')
     worksheet = workbook.add_worksheet()
     text_format = workbook.add_format({'text_wrap': True})
     worksheet.set_column('A:A', 50)
     worksheet.set_column('B:B', 50)
     worksheet.set_column('C:C', 50)
     worksheet.set_column('D:D', 30)
+    worksheet.set_column('E:E', 30)
     
    # print(mline)
     worksheet.write('A1', "MACRO NAME")
-    worksheet.write('B1', "CSV Filename")
-    worksheet.write('C1', "Code Filenames")
-    worksheet.write('D1', "Line of Code")
-    worksheet.write('E1', "Duplicate Macro Count (found:" + str(len(dup_found)) +")")
+    #worksheet.write('B1', "CSV Filename")
+    worksheet.write('B1', "Code Filenames")
+    worksheet.write('C1', "Line of Code")
+    worksheet.write('D1', "Duplicate Macro Count (found:" + str(len(dup_found)) +")")
     
     ex_len = len(mname)
     
     for e in range(ex_len):
         worksheet.write('A' + str(e+2), str(mname[e]), text_format) 
         
-        tstr1 = str(result_csvfile[e])
-        tstr = tstr1.replace('[','')   
-        tstr2 = tstr.replace(']','')
+        #tstr1 = str(result_csvfile[e])
+        #tstr = tstr1.replace('[','')   
+        #tstr2 = tstr.replace(']','')
         #worksheet.write('B' + str(e+2),tstr2, text_format) # str(result_csvfile[e]))
-        worksheet.write('B' + str(e+2), str(result_csvfile[e]), text_format)
-        worksheet.write('C' + str(e+2), str(mfile[e]), text_format)
-        worksheet.write('D' + str(e+2), str(mline[e]),text_format)
-        worksheet.write('E' + str(e+2), str(len(mline[e])),text_format)
-        
+        #worksheet.write('B' + str(e+2), str(result_csvfile[e]), text_format)
+        worksheet.write('B' + str(e+2), str(mfile[e]), text_format)
+        worksheet.write('C' + str(e+2), str(mline[e]),text_format)
+        worksheet.write('D' + str(e+2), str(len(mline[e])),text_format)
+        if len(mline[e]) > 1:
+             worksheet.write('E' + str(e+2), "DUPLICATE FOUND",text_format)
+        else:
+             worksheet.write('E' + str(e+2), "NO DUPLICATE",text_format)
+
+    worksheet.write('A' + str(e+4), "TOTAL NUMBER of MACRO", text_format)
+    worksheet.write('B' + str(e+4),str(ex_len), text_format)   
+    worksheet.write('A' + str(e+5), "TOTAL NUMBER of DUPLICATE MACRO", text_format)
+    worksheet.write('B' + str(e+5),str(len(dup_found)), text_format)
+
     workbook.close()
 
 if __name__ == '__main__':
@@ -112,9 +129,10 @@ if __name__ == '__main__':
     for x in os.listdir():
       if x.endswith(".csv"):
         # Prints only text file present in My Folder
-        process_file(str(x)) 
+        process_file(str(x))
+        macro_export(result_macroname,result_filename,result_line_number,str(x)) 
     
-    macro_export(result_macroname,result_filename,result_line_number)
+    
     
     
     #process_file("Adc_Internal.c.macros.csv") 
@@ -127,10 +145,10 @@ if __name__ == '__main__':
         #print(result_macroname[rdx] + " | " + "count:" + str(len(result_filename[rdx])-1)  + "  " +  str(result_filename[rdx])) # + " | "  + result_line_number[d] )
    #    print(result_macroname[rdx] + " | " + "Duplicate:" + str(len(result_filename[rdx])-1) )# + "  " +  str(result_filename[rdx])) # + " | "  + result_line_number[d] )
     
-    print("============================================")    
-    print("\nDuplicate count:" + str(len(dup_found)))    
-    print("Total macro: " + str(len(result_macroname)))
-    print("============================================")    
+   # print("============================================")    
+   # print("\nDuplicate count:" + str(len(dup_found)))    
+   # print("Total macro: " + str(len(result_macroname)))
+   # print("============================================")    
     print("see output result on file: result.xlsx")
     input("\n\nPress enter to continue")
     
